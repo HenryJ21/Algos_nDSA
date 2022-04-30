a5af1bfa51e0e886b510d848fcb13581d86e1c8f
HenryJ21 <100504139+HenryJ21@users.noreply.github.com>
2022-04-30 18:30:04 +0530

    Update 4_countSubsetswithGivenSumGFG.cpp

--- DP/1_01Knapsack/4_countSubsetswithGivenSumGFG.cpp
+++ DP/1_01Knapsack/4_countSubsetswithGivenSumGFG.cpp
@@ -1,48 +1,38 @@
 //link is https://practice.geeksforgeeks.org/problems/perfect-sum-problem5633/1
 
 
-class Solution {
-public:
-    bool canPartition(vector<int>& nums) {
-        int sum = 0;
-        int n = nums.size();
+class Solution{
+
+	public:
+	int perfectSum(int arr[], int n, int sum)
+	{
+        int moda = 1000000007;
         
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
+        int dp[n+1][sum+1];
+        dp[0][0] = 1;
+        int counter = 0;
+        for(int i=1;i<=n;i++){
+            if(arr[i-1]==0){
+                counter++;
+            }
+            dp[i][0] = pow(2,counter);
+        }
+        for(int j=1;j<=sum;j++){
+            dp[0][j] = 0;
         }
         
-        if(sum%2==0){
-            sum = sum/2;
-            int dp[n+1][sum+1];
-            for(int i=0;i<=n;i++){
-                dp[i][0] = 1;
-            }
+        for(int i=1;i<=n;i++){
             for(int j=1;j<=sum;j++){
-                dp[0][j] = 0;
-            }
-            
-            for(int i=1;i<=n;i++){
-                for(int j=1;j<=sum;j++){
-                    if(nums[i-1]<=j){
-                        dp[i][j] = max(dp[i-1][j-nums[i-1]], dp[i-1][j]);
-                    }
-                    else{
-                        dp[i][j] = dp[i-1][j];
-                    }
+                if(arr[i-1]<=j){
+                    dp[i][j] = (dp[i-1][j-arr[i-1]]%moda+dp[i-1][j]%moda)%moda;
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
                 }
             }
-            
-            if(dp[n][sum]==1){
-                return true;
-            }
-            else{
-                return false;
-            }
-            
-        }
-        else{
-            return false;
         }
         
-    }
+        return dp[n][sum];
+	}
+	  
 };
